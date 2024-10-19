4e1991671fcc919f3c1579aa6ca3af07930b711e
Joseph Ruiz <ruizdev7@outlook.com>
2024-10-19 13:23:37 +0200

    Complete chapter two variables

---
+++ variables2.js
@@ -0,0 +1,48 @@
+/*
+
+Scenario
+
+Our task will be to create a list of contacts. 
+Initially, the list will be quite simple: we will only write three people to it using the data shown in the table below. 
+In the rest of the course, you will return to this script and systematically extend it with new functionality, 
+using the newly learned elements of JavaScript.
+
+Name	Phone	Email
+Maxwell Wright	(0191) 719 6495	Curabitur.egestas.nunc@nonummyac.co.uk
+Raja Villarreal	0866 398 2895	posuere.vulputate@sed.com
+Helen Richards	0800 1111	libero@convallis.edu
+
+Declare and initialize the variables where you will store all the information (nine variables in total). 
+Display in the console information about the first and last contact in the form: name/phone/email.
+
+*/
+
+let MaxwellName = "Maxwell Write";
+let MaxwellPhone = 1917196495;
+let MaxwellEmail = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let RajaName = "Raja Villareal";
+let RajaPhone = 8663982895;
+let RajaEmail = "posuere.vulputate@sed.com";
+
+let HelenName = "Helen Richards";
+let HelenPhone = 8001111;
+let HelenEmail = "libero@convallis.edu";
+
+console.log(
+  "Maxwell – Name:",
+  MaxwellName,
+  ", MMaxwell Phone:",
+  MaxwellPhone,
+  ", Maxwell Email:",
+  MaxwellEmail
+);
+
+console.log(
+  "Helen – Name:",
+  HelenName,
+  ", Helen Phone:",
+  HelenPhone,
+  ", Helen Email:",
+  HelenEmail
+);
